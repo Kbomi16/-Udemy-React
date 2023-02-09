ac78bf38cf61533d33c4b2a592cb3d6cfe26ac21
김보미 <kimbomi2172@naver.com>
2023-02-09 17:40:54 +0900

    Section 4

--- README.md
+++ README.md
@@ -1,5 +1,57 @@
 # -Udemy-React
 
+✔️ Props
+- 사용법
+1️⃣컴포넌트 내부에서 코드를 작성한다.
+```js
+<Hello name="mark">
+```
+2️⃣받는쪽은 props 라는 예약어를 통해 받는다.
+```js
+function Hello(props) {
+  return <div>{props.name}</div>
+}
+```
+✔️ defaultProps
+- 컴포넌트에 props 를 지정하지 않았을 때 기본적으로 사용 할 값을 설정함.
+```js
+function Hello({ color, name }) {
+  return <div style={{ color }}>안녕하세요 {name}</div>
+}
+
+Hello.defaultProps = {
+  name: '이름없음'
+}
+```
+```js
+function App() {
+  return (
+    <>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </>
+  );
+}
+```
+https://i.imgur.com/WXSoZyf.png 
+
+✔️ Props.children
+- 컴포넌트 태그 사이에 값이 있을때 'children'이란 예약어를 사용한다.
+```js
+function Wrapper({ children }) {
+  const style = {
+    border: '2px solid black',
+    padding: '16px',
+  };
+  return (
+    <div style={style}>
+      {children}
+    </div>
+  )
+}
+```
+
+
 ✔️ setState <br>
 - setState()는 컴포넌트의 state 객체에 대한 업데이트를 실행한다. 
 - state가 변경되면, 컴포넌트는 리렌더링된다.
---
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,98 @@
+import React, {useState} from "react";
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState ({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate : ''
+  // });
+
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+
+    // setUserInput({
+    //   // 스프레드 연산자
+    //   ...userInput, 
+    //   enteredTitle: event.target.value,
+    // })
+
+    // 동시에 수많은 상태 업데이트를 한다면 오래됐거나 잘못된 상태 스냅샷에 의존할 수 있음.
+    // 아래 방법은 항상 최신상태의 스냅샷을 유지함.(항상 계획된 상태 업데이트)
+    //  setUserInput((prevState) => {
+    //     return {...prevState, enteredTitle: event.target.value};
+    //   });
+    // 
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   // 스프레드 연산자
+    //   ...userInput, 
+    //   enteredAmount: event.target.value,
+    // })
+  
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   // 스프레드 연산자
+    //   ...userInput, 
+    //   enteredDate: event.target.value,
+    // })
+  };
+
+const submitHandler = (event) => {
+  // 기본 요청 방지
+  event.preventDefault();
+
+  const expenseData = {
+    title: enteredTitle,
+    amount: enteredAmount,
+    //내장된 날짜 생성자로 새로운 날짜를 구성함. 
+    // 해당 날짜 문자열을 분석해서 날짜 객체로 변환 후 enteredDate로 전달함.
+    date: new Date(enteredDate) 
+  };
+
+  props.onSaveExpenseData(expenseData);
+
+  // 입력창 reset
+  setEnteredTitle('');
+  setEnteredAmount('');
+  setEnteredDate('');
+}
+
+  return(
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' value={enteredAmount}  onChange={amountChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler}/>
+        </div>
+      </div>
+
+      <div className="new-expense__actions">
+        <button type="submit" >Add Expense</button>
+      </div>
+
+    </form>
+  );
+}
+
+export default ExpenseForm;
